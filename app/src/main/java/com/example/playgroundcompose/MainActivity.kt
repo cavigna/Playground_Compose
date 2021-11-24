fe4c0ab41cb17b3218c78b567e67eec024120e0d
cavigna <40524472+cavigna@users.noreply.github.com>
2021-11-24 20:34:18 -0300

    Added Card

--- app/src/main/java/com/example/playgroundcompose/MainActivity.kt
+++ app/src/main/java/com/example/playgroundcompose/MainActivity.kt
@@ -7,9 +7,11 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.playgroundcompose.ui.compose.Mostrarartjeta
 import com.example.playgroundcompose.ui.theme.PlaygroundComposeTheme
+import com.example.playgroundcompose.ui.theme.blueBackground
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,7 +19,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             PlaygroundComposeTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
+                Surface(color = blueBackground) {
                     Mostrarartjeta()
                 }
             }
--- app/src/main/java/com/example/playgroundcompose/ui/compose/Tarjeta.kt
+++ app/src/main/java/com/example/playgroundcompose/ui/compose/Tarjeta.kt
@@ -2,59 +2,58 @@ package com.example.playgroundcompose.ui.compose
 
 import android.content.res.Resources
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.playgroundcompose.ui.theme.Teal200
-import com.example.playgroundcompose.ui.theme.greenCard
-import com.example.playgroundcompose.ui.theme.purpleCard
+import androidx.compose.ui.unit.sp
+import com.example.playgroundcompose.ui.theme.*
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun Mostrarartjeta(
     modifier: Modifier = Modifier
-){
+) {
     Column(
-        modifier = modifier.fillMaxSize()
+        //modifier = modifier.fillMaxSize()
     ) {
 
-        Row{
-            Column(
-                modifier = modifier.fillMaxSize()
-            ) {
-                Tarjeta()
-                Tarjeta(color = purpleCard)
-
-            }
+        Row(verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.Center) {
+            Text(
+                "Indicadores De Chile",
+                fontSize = 50.sp,
+                fontFamily = FontFamily.Monospace,
+                color = Color.White,
 
 
+            )
         }
 
-        Row{
-            Column(
-                modifier = modifier.fillMaxSize()
-            ) {
+        Row(modifier = modifier.fillMaxSize()) {
+            Spacer(modifier.size(50.dp))
+            Column {
                 Tarjeta()
-                Spacer(modifier.size(50.dp))
-                Tarjeta(color = purpleCard)
-                Tarjeta(color = purpleCard)
-                Tarjeta(color = purpleCard)
-                Tarjeta(color = purpleCard)
-                Tarjeta(color = purpleCard)
-                Tarjeta(color = purpleCard)
+                Tarjeta(color = orangeCard)
+                Tarjeta(color = orangeCard)
+
             }
+            Column {
+                Tarjeta(color = purpleCard)
+                Tarjeta(color = pinkCard)
+                Tarjeta(color = pinkCard)
 
-
+            }
         }
-
 
 
     }
+
 
 }
 
@@ -63,19 +62,19 @@ fun Tarjeta(
     modifier: Modifier = Modifier,
     color: Color = greenCard,
     monto: String = "123"
-){
-    Surface(modifier = modifier.padding(10.dp)){
+) {
+    Surface(modifier = modifier.padding(10.dp), color = blueBackground) {
         Card(
             modifier = modifier.size(175.dp, 125.dp).padding(5.dp),
             shape = RoundedCornerShape(10.dp),
             backgroundColor = color,
 
-        ) {
+            ) {
             Column(
                 modifier.fillMaxWidth(),
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally
-            ){
+            ) {
                 Text(monto)
             }
         }
