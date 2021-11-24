18b157a0dae08373681ff8da679a68479b3ec4eb
cavigna <40524472+cavigna@users.noreply.github.com>
2021-11-24 14:12:00 -0300

    Added Card

--- app/src/main/java/com/example/playgroundcompose/ui/compose/Tarjeta.kt
+++ app/src/main/java/com/example/playgroundcompose/ui/compose/Tarjeta.kt
@@ -1,29 +1,84 @@
 package com.example.playgroundcompose.ui.compose
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Card
-import androidx.compose.material.Text
+import android.content.res.Resources
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.playgroundcompose.ui.theme.Teal200
+import com.example.playgroundcompose.ui.theme.greenCard
+import com.example.playgroundcompose.ui.theme.purpleCard
 
 @Preview
 @Composable
-fun Mostrarartjeta(){
-    Tarjeta()
+fun Mostrarartjeta(
+    modifier: Modifier = Modifier
+){
+    Column(
+        modifier = modifier.fillMaxSize()
+    ) {
+
+        Row{
+            Column(
+                modifier = modifier.fillMaxSize()
+            ) {
+                Tarjeta()
+                Tarjeta(color = purpleCard)
+
+            }
+
+
+        }
+
+        Row{
+            Column(
+                modifier = modifier.fillMaxSize()
+            ) {
+                Tarjeta()
+                Spacer(modifier.size(50.dp))
+                Tarjeta(color = purpleCard)
+                Tarjeta(color = purpleCard)
+                Tarjeta(color = purpleCard)
+                Tarjeta(color = purpleCard)
+                Tarjeta(color = purpleCard)
+                Tarjeta(color = purpleCard)
+            }
+
+
+        }
+
+
+
+    }
+
 }
 
 @Composable
 fun Tarjeta(
     modifier: Modifier = Modifier,
+    color: Color = greenCard,
     monto: String = "123"
 ){
-    Card(
-        modifier = modifier
-    ) {
-        Column(modifier.fillMaxWidth()){
-            Text(monto)
+    Surface(modifier = modifier.padding(10.dp)){
+        Card(
+            modifier = modifier.size(175.dp, 125.dp).padding(5.dp),
+            shape = RoundedCornerShape(10.dp),
+            backgroundColor = color,
+
+        ) {
+            Column(
+                modifier.fillMaxWidth(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+                Text(monto)
+            }
         }
     }
+
 }
